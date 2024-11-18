eb96aabaaa0ce523bdf7a646e6785e3df4d95c16
4sdk <bobo19700@gmail.com>
2024-11-18 13:43:59 +0200

    added convert_rows_to_directory function

--- ConvertToJson/convert_csv_to_json.py
+++ ConvertToJson/convert_csv_to_json.py
@@ -6,3 +6,12 @@ def open_csv_file(csv_path):
     with open(csv_path, encoding='utf-8') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         return csv_reader
+
+
+def convert_rows_to_dictionary(csv_reader):
+    data = {}
+    for row in csv_reader:
+        key = row['IDNum']
+        data[key] = row
+
+    return data
