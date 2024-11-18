e556e8cb29e96ea77d964f9c76dba92abe9c6f61
4sdk <bobo19700@gmail.com>
2024-11-18 13:54:55 +0200

    added main function to convert_csv_to_json.py

--- ConvertToJson/convert_csv_to_json.py
+++ ConvertToJson/convert_csv_to_json.py
@@ -20,3 +20,16 @@ def convert_rows_to_dictionary(csv_reader):
 def write_to_json(json_path, data):
     with open(json_path, 'w', encoding='utf-8') as json_file:
         json_file.write(json.dumps(data, indent=4))
+
+
+def main():
+    csv_path = r'MadaReports - MadaReports.csv'
+    json_path = ''
+
+    csv_reader = open_csv_file(csv_path)
+    data = convert_rows_to_dictionary(csv_reader)
+    write_to_json(json_path, data)
+
+
+if __name__ == '__main__':
+    main()
