9caa3018e82b71f6367844cacbbf22663bd377b6
tomerE88 <bobo19700@gmail.com>
2024-11-18 18:48:42 +0200

    creating different json file when reaching maximum

--- ConvertToJson/convert_csv_to_json.py
+++ ConvertToJson/convert_csv_to_json.py
@@ -18,10 +18,10 @@ def open_csv_file_to_get_data(csv_path):
 
 def convert_rows_to_dictionary(csv_reader):
     data = {}
-    counter = 0
+    counter = configuration_variables['rows']['first_row']
     for row in csv_reader:
         key = counter
-        counter += 1
+        counter += configuration_variables['rows']['next_row']
         data[key] = row
 
     return data
@@ -36,18 +36,16 @@ def check_maximum_rows(data):
     return True if num_rows <= configuration_variables['rows']['max'] else False
 
 
-def write_to_json(json_path, data, index):
+def write_to_json(json_path, data, index_min, index_max):
     with open(json_path, configuration_variables['files']['write'],
               encoding=configuration_variables['encode']['eight_bit']) as json_file:
-        if index == configuration_variables['rows']['max']:
-            return
-        if index < configuration_variables['rows']['max']:
-            small_index = index
-            index = configuration_variables['rows']['max']
-        else:
-            small_index = index - configuration_variables['rows']['max']
-        for i in range(small_index, index):
+
+        if index_max > count_data_rows(data):
+            index_max = count_data_rows(data)
+
+        for i in range(index_min, index_max):
             print(i)
+            print(json_path)
             json_file.write(json.dumps(data[i], indent=configuration_variables['json']['space_between_rows']))
 
 
@@ -64,14 +62,16 @@ def main():
 
     create_folder(configuration_variables['json']['path']['folder'])
     data = open_csv_file_to_get_data(csv_path)
+    file_number = configuration_variables['files']['first_file']
     if check_maximum_rows(data):
-        write_to_json(configuration_variables['json']['path']['file'], data, 0)
+        json_path = f"{configuration_variables['json']['path']['file']}_{file_number}"
+        write_to_json(json_path, data, configuration_variables['rows']['first'], configuration_variables['rows']['max'])
     else:
-        for index in range(0, count_data_rows(data), configuration_variables['rows']['max']):
-            write_to_json(configuration_variables['json']['path']['file'], data, index)
-
-        write_to_json(configuration_variables['json']['path']['file'], data,
-                      count_data_rows(data) - configuration_variables['rows']['max'])
+        for index in range(configuration_variables['rows']['first'], count_data_rows(data),
+                           configuration_variables['rows']['max']):
+            json_path = f"{configuration_variables['json']['path']['file']}_{file_number}"
+            write_to_json(json_path, data, index, index + configuration_variables['rows']['max'])
+            file_number += configuration_variables['files']['next_file']
 
 
 if __name__ == '__main__':
